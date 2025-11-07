ecbbc759918227781ca6f9c9e2d8d582f76d0794
Francisco235711 <josehernandezmolina2007@gmail.com>
2025-11-07 11:42:30 -0300

    al parecer se pudo quitar el error 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import MapaVirtudes from "./components/MapaVirtudes";
 import HeroesArdientes from "./components/HeroesArdientes";
